8b9beded052b0eaeb16c6722ecfbf79a124a71c6
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-04 15:24:56 +0300

    Javascript_Odev-2 tamamlandi.

--- Javascript_Odev-2/js/script.js
+++ Javascript_Odev-2/js/script.js
@@ -3,19 +3,37 @@ function addItem(){
   const item = document.querySelector("#task").value;
   if(item && item.trim().length){
     const liDOM = document.createElement("li");
-    liDOM.innerHTML = `${item}<span class="close" onclick="removeItem()">×</span>`;
+    liDOM.innerHTML = `${item}<span class="close">×</span>`;
     ulDOM.append(liDOM);
+    $(".success").toast("show");
+    removeItem();
+  }else{
+    $(".error").toast("show");
   }
 
   const inputDOM = document.querySelector("#task");
-  inputDOM.value = "";
+  inputDOM.value = ""; 
 }
 
 function removeItem(){
-  let close = document.querySelector(".close");
-  console.log(close)
+  const closeDOM = document.querySelectorAll("span.close");
+  for(let i = 0; i <closeDOM.length; i++){
+    closeDOM[i].onclick = function(){
+      let div = this.parentElement;
+      div.remove();
+    }
+  }
 }
 
 function done(){
-
+  const ul_li_DOM = document.querySelectorAll("#list>li");
+  for(let i = 0; i < ul_li_DOM.length; i++){
+    ul_li_DOM[i].onclick = function(){
+      this.classList.toggle("checked");
+    }
+  }
+  removeItem();
 }
+
+done();
+removeItem();
