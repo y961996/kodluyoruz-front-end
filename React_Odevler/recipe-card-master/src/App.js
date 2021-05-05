4e6564f8b1cb1a6e3f7e099655bc8f4cce332d75
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-05 14:24:02 +0300

    React_Odev-1 tamamlandi.

--- React_Odevler/recipe-card-master/src/App.js
+++ React_Odevler/recipe-card-master/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Card from "./components/Card/Card";
 import food from "./assets/food.jpg"; // Tell webpack this JS file uses this image
+import food2 from "./assets/ratatuy.jpg";
 
 function App() {
   const recipeAuthor = "Kodluyoruz";
@@ -12,8 +13,20 @@ function App() {
       "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
   };
 
+  const recipeAuthor2 = "Fare";
+  const recipeItem2 = {
+    title: "Ratatuy",
+    date: "5 Mayıs 2021, Çarşamba",
+    image: food2,
+    description:
+      "Ratatuy, bir farenin ellerinden çıkan lezzet.",
+  };
+
   const likeCount = 193;
-  const isLiked = false;
+  const isLiked = true;
+
+  const likeCount2 = 1566842;
+  const isLiked2 = false;
 
   return (
     <div className="App">
@@ -21,6 +34,22 @@ function App() {
         <Card
           /* prop ismi = { değişken } */
           author={recipeAuthor}
+          title={recipeItem.title}
+          date={recipeItem.date}
+          image={recipeItem.image}
+          description={recipeItem.description}
+          likeCount={likeCount}
+          liked={isLiked}
+        />
+        <Card
+          /* prop ismi = { değişken } */
+          author={recipeAuthor2}
+          title={recipeItem2.title}
+          date={recipeItem2.date}
+          image={recipeItem2.image}
+          description={recipeItem2.description}
+          likeCount={likeCount2}
+          liked={isLiked2}
         />
       </header>
     </div>
